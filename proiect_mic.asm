f6534d365dddd029e076b7c8c32679e38113eacf
Radu-24 <115228230+Radu-24@users.noreply.github.com>
2024-11-19 13:30:15 +0200

    UI Aproape Compet ASM

--- proiect_mic.asm
+++ proiect_mic.asm
@@ -4,24 +4,43 @@ org 100h
 
 .model small
 .stack 100h
+
 .data
-; Title Section
-msg_title_top db "+==============================================================================+$", 0
-msg_title_inner db "|                              Parking Meter System                            |$", 0
-msg_title_bottom db "+==============================================================================+$", 0
+; Titlul
+msg_title_top db "+==============================================================================+$", 10, 13
+msg_title_inner db "||                          -Parcometru in Functiune-                         ||$", 10, 13
+msg_title_bottom db "+==============================================================================+$", 10, 13
+
+; Lista numerelor de înmatriculare, zonele ?i costurile
+msg_db db "+------------------------------+     +----Zone----+    +--------Price--------+", 10, 13
+        db " | PH 41 CRC: Afara           |            Z0           |  1 ora: 10 Lei    |", 10, 13
+        db " | PH 87 MOX: Afara           |                                              ", 10, 13
+        db " | BV 34 MIS: Afara           |            Z1           |  2 ore: 15 Lei    |", 10, 13
+        db " | B 146 SYS: Afara           |                                              ", 10, 13
+        db " | BV 89 DXP: Afara           |            Z2           |  24 ore: 50 Lei   |", 10, 13
+        db "+------------------------------+     +------------+    +---------------------+", 10, 13
+
+; Linie separatoare pentru intrari
+msg_separator db "+==============================================================================+$", 10, 13
+
+; Input rectangles
+msg_input_top db "+-------------------------------+ +---------------------+ +--------------------+$", 10, 13
+msg_input_nr db "| Introdu nr. de inmatriculare: | |    Introdu zona:    | |  Introdu timpul:   |$", 10, 13
+msg_input_empty db "|                               | |                     | |                    |$", 10, 13
+msg_input_bottom db "+-------------------------------+ +---------------------+ +--------------------+$", 10, 13
 
 .code
 main proc
-    ; Initialize data segment
+    ; Ini?ializeaza segmentul de date
     mov ax, @data
     mov ds, ax
 
-    ; Clear the screen
-    mov ah, 0   ; Set video mode
-    mov al, 3   ; 80x25 text mode
+    ; Cura?a ecranul
+    mov ah, 0   ; Seteaza modul video
+    mov al, 3   ; Modul text 80x25
     int 10h
 
-    ; Display the title
+    ; Afi?eaza titlul
     lea dx, msg_title_top
     call print_message
     lea dx, msg_title_inner
@@ -29,14 +48,32 @@ main proc
     lea dx, msg_title_bottom
     call print_message
 
-    ; End program
+    ; Afi?eaza lista numerelor de înmatriculare cu zonele ?i costurile
+    lea dx, msg_db
+    call print_message
+
+    ; Afi?eaza separatorul înaintea dreptunghiurilor de intrare
+    ;lea dx, msg_separator
+    ;call print_message
+
+    ; Afi?eaza dreptunghiurile pentru introducerea datelor
+    lea dx, msg_input_top
+    call print_message
+    lea dx, msg_input_nr
+    call print_message
+    lea dx, msg_input_empty
+    call print_message
+    lea dx, msg_input_bottom
+    call print_message
+
+    ; Termina programul
     mov ah, 4Ch
     int 21h
 main endp
 
-; Print a null-terminated message
+; Procedura pentru afi?area unui mesaj
 print_message proc
-    mov ah, 09h
+    mov ah, 09h    ; Func?ia de afi?are a ?irului
     int 21h
     ret
 print_message endp
